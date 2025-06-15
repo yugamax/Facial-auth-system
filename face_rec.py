6bbd96c7f2a1d628ff082424aeed420db9b5f7c2
yugamax <ayp14111@gmail.com>
2025-06-16 02:23:27 +0530

    Update .gitignore, modify uvicorn host in face_rec.py, and add requirements.txt

--- face_rec.py
+++ face_rec.py
@@ -81,4 +81,4 @@ async def verify_user(
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="127.0.0.1", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
