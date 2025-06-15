fe69f422d58605f785744cd4589346ed77733f21
yugamax <ayp14111@gmail.com>
2025-06-16 01:37:04 +0530

    Initialize db_handling.py and db_init.py for database handling

---
+++ db_handling.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, String, JSON, DateTime
+from sqlalchemy.sql import func
+from .database import Base
+
+class FaceEncoding(Base):
+    __tablename__ = "face_encodings"
+
+    username = Column(String, primary_key=True, index=True)
+    encoding = Column(JSON, nullable=False)
+    registered_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
